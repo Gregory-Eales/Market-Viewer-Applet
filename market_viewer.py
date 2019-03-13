ab9540e55ec561886fe41465060200aab7a39468
Gregory Eales <gregoeales@aol.com>
2019-03-13 00:45:07 -0700

    added api

--- market_viewer.py
+++ market_viewer.py
@@ -1,1 +1,25 @@
+from tkinter import *
+from alpha_vantage_api import AlphaVantageAPI
+import pandas
+
+
+
+class MarketViewer(object):
+
+    def __init__(self):
+
+        api_key = "demo"
+        self.api = AlphaVantageAPI(api_key=api_key)
+        self.gui = Tk()
+        self.gui.title("Market Viewer")
+        self.calc_text = Label(self.gui, text=self.text, bd=1, relief=SUNKEN, anchor=N)
+        self.calc_text.grid(column=1, columnspan=8, row=0)
+        self.frame = Frame(self.gui, width=600, height=1800)
+        self.gui.mainloop()
+
+
+
+
+
+
 
